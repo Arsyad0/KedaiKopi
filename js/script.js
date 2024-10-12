5d8350c83c0ac4a718b8f804927e2a19b7539bb5
Arsyad0 <151544969+Arsyad0@users.noreply.github.com>
2024-10-12 19:39:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+//icon eye
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const itemDetailModal = document.getElementById('item-detail-modal');
+  const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+  const closeModalButton = document.querySelector('.close-icon');
+
+  function showModal() {
+    itemDetailModal.style.display = 'block';
+  }
+
+  function hideModal() {
+    itemDetailModal.style.display = 'none';
+  }
+
+  itemDetailButtons.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      showModal();
+    });
+  });
+
+  closeModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    hideModal();
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target === itemDetailModal) {
+      hideModal();
+    }
+  });
+});
